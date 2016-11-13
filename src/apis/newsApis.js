7cdb64e43a71bb79633db7c9b7b5b50daa62a305
Victoria Sloan <victoriasloan95@gmail.com>
2016-11-13 01:08:20 +0000

    News sources added

--- src/apis/newsApis.js
+++ src/apis/newsApis.js
@@ -1,11 +1,16 @@
 import $ from 'jquery';
 import {
-    NEWS_API_URL,
+    NEWS_ARTICLES_URL,
+    NEWS_SOURCES_URL,
     NEWS_API_KEY
 } from 'constants/apiConstants';
 
 const getNewsStoriesFromSource = (source, sortBy) => $.getJSON(`${NEWS_API_URL}?source=${source}&sortBy=${sortBy}&apiKey=${NEWS_API_KEY}`);
 
+const getNewsSources = () => $.getJSON(`${NEWS_SOURCES_URL}`);
+
+
 export default {
-    getNewsStoriesFromSource
+    getNewsStoriesFromSource,
+    getNewsSources
 }
--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,7 +1,9 @@
-const NEWS_API_URL = 'https://newsapi.org/v1/articles';
+const NEWS_ARTICLES_URL = 'https://newsapi.org/v1/articles';
+const NEWS_SOURCES_URL = 'https://newsapi.org/v1/sources?language=en'
 const NEWS_API_KEY = 'aafc8033648c4127b57709449c91548e';
 
 export {
-    NEWS_API_URL,
+    NEWS_ARTICLES_URL,
+    NEWS_SOURCES_URL,
     NEWS_API_KEY
 }
